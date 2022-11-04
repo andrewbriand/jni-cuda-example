2db8c0fbffa6f778c118a8d7ce9cfa525fcba479
Andrew Briand <abriand@nvidia.com>
2022-11-04 08:33:42 -0700

    CUDA error checking

--- VectorAdd.cu
+++ VectorAdd.cu
@@ -1,8 +1,20 @@
 #include <vector>
+#include <iostream>
 
 #include <jni.h>
 
 #include "VectorAdd.h"
+
+#define CHK_CUDA(expression)                                                  \
+  {                                                                           \
+    cudaError_t status = (expression);                                        \
+    if (status != cudaSuccess) {                                              \
+      std::cerr << "Error in file: " << __FILE__ << ", on line: " << __LINE__ \
+                << ": " << cudaGetErrorString(status) << std::endl;           \
+      std::exit(EXIT_FAILURE);                                                \
+    }                                                                         \
+  }
+
 
 __global__ void vector_add_kernel(int *a, int *b, int *c, size_t len) {
 
@@ -25,23 +37,25 @@ JNIEXPORT jintArray JNICALL Java_VectorAdd_add(JNIEnv *env, jclass thisClass,
 
   int *d_a, *d_b, *d_c;
 
-  cudaMalloc(&d_a, sizeof(int) * len);
-  cudaMalloc(&d_b, sizeof(int) * len);
-  cudaMalloc(&d_c, sizeof(int) * len);
+  CHK_CUDA(cudaMalloc(&d_a, sizeof(int) * len));
+  CHK_CUDA(cudaMalloc(&d_b, sizeof(int) * len));
+  CHK_CUDA(cudaMalloc(&d_c, sizeof(int) * len));
 
-  cudaMemcpy(d_a, h_a, sizeof(int) * len, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b, h_b, sizeof(int) * len, cudaMemcpyHostToDevice);
+  CHK_CUDA(cudaMemcpy(d_a, h_a, sizeof(int) * len, cudaMemcpyHostToDevice));
+  CHK_CUDA(cudaMemcpy(d_b, h_b, sizeof(int) * len, cudaMemcpyHostToDevice));
 
   const int block_size = 32;
   const int num_blocks = len / block_size + 1;
 
   vector_add_kernel<<<num_blocks, block_size>>>(d_a, d_b, d_c, len);
 
-  cudaMemcpy(h_c.data(), d_c, sizeof(int) * len, cudaMemcpyDeviceToHost);
+  CHK_CUDA(cudaGetLastError());
 
-  cudaFree(d_a);
-  cudaFree(d_b);
-  cudaFree(d_c);
+  CHK_CUDA(cudaMemcpy(h_c.data(), d_c, sizeof(int) * len, cudaMemcpyDeviceToHost));
+
+  CHK_CUDA(cudaFree(d_a));
+  CHK_CUDA(cudaFree(d_b));
+  CHK_CUDA(cudaFree(d_c));
 
   env->ReleaseIntArrayElements(a, h_a, 0);
   env->ReleaseIntArrayElements(b, h_b, 0);
